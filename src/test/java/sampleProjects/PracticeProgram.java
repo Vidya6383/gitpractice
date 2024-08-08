178fb9fa1bb163bcb44b64466c7f5b543ea4d6d2
Vidya6383 <vidyag797@gmail.com>
2024-08-08 16:04:54 +0530

    2 commit

--- src/test/java/sampleProjects/PracticeProgram.java
+++ src/test/java/sampleProjects/PracticeProgram.java
@@ -5,5 +5,7 @@ public class PracticeProgram {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		//completed.......
+		
+		System.out.println("Github launched successfully");
 	}
 }

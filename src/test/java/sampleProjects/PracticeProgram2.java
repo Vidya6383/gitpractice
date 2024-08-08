349e20b47e7f9117575f460645ae05d83c489217
Vidya6383 <vidyag797@gmail.com>
2024-08-08 15:46:09 +0530

    1st commit

---
+++ src/test/java/sampleProjects/PracticeProgram2.java
@@ -0,0 +1,11 @@
+package sampleProjects;
+
+public class PracticeProgram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+
+}
